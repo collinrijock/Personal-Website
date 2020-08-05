1f925d3ed55433b87d848c9eaf3d854db7638875
Collin Rijock <collin@Collins-MacBook-Pro.local>
2020-08-05 15:45:03 -0400

    'rough splash page and testing big script'

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -3,5 +3,11 @@ git add .
 echo "Enter commit msg:"
 read msg
 git commit -am "'$msg'"
-git push remote master
-echo "Changes deployed."
+git push
+echo "Changes pushed"
+ssh collin@collinrijock.dev
+cd /var/www/collinrijock.dev/Personal-Website
+git pull
+npm run build
+exit
+echo "Changes deployed"
--- src/MainPage.vue
+++ src/MainPage.vue
@@ -1,6 +1,7 @@
 <template>
      <div id="main-page">
-          <h1>COLLIN</h1>
+          <img src="./assets/cpr_logo.svg"></img>
+          <h1>Collin Rijock</h1>
      </div>
 </template>
 
@@ -21,5 +22,33 @@ export default {
 </script>
 
 <style lang="scss">
+@import url("./assets/Tofino Pro Wide/stylesheet.css");
+.clear {
+     margin: 0px;
+     padding: 0px;
+}
+body {
+     @extend .clear;
+}
+#main-page {
+     background: #FFEA46;
+     height: 100vh;
+     width: 100vw;
+     display: grid;
+     place-items: center;
+     position: relative;
+     img {
+          height: 6%;
+          position: absolute;
+          top: 6%;
+          left: 6%;
+
+     }
+     h1 {
+          font-family: 'tofino';
+          color: #65002F;
+          font-size: 4rem;
+     }
+}
 
 </style>
