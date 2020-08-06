79ee38ae1429e5aeba4082b9bf48e544671fa905
Collin Rijock <collin@Collins-MacBook-Pro.local>
2020-08-06 12:26:04 -0400

    '.'

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -5,5 +5,5 @@ read msg
 git commit -am "'$msg'"
 git push
 echo "Changes pushed"
-ssh collin@collinrijock.dev 'bash -s' < serverUpdate.sh
+ssh collin@collinrijock.dev 'bash -s' < ./serverUpdate.sh
 echo "Changes deployed"
