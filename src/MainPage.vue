b9ddd38ba605802cb93053f87058f4b644b82d3d
collinrijock <collinrijock@gmail.com>
2020-09-06 23:00:30 -0400

    'New simple design'

--- src/MainPage.vue
+++ src/MainPage.vue
@@ -1,171 +1,79 @@
 <template>
-     <div id="main-page" >
-          <section id="splash" data-relative-input="true" data-pointer-events="false">
-               <img data-depth="0.1" id="logo" src="./assets/cpr_logo.svg"></img>
-               <img data-depth="0.1" class="box" src="./assets/whitebox.svg">
-               <div data-depth="0.6" id="first-name">
-                    <span>C</span><span>o</span><span>l</span><span>l</span><span>i</span><span>n</span>
-               </div>
-               <div data-depth="0.6" id="last-name">
-                    <span>R</span><span>i</span><span>j</span><span>o</span><span>c</span><span>k</span><span>.</span>
-               </div>
-               <div data-depth="0.6" id="subheader">Software Engineer</div>
-          </section>
+     <div id="main-page">
           <section id="hero">
-               <div id="about-container">
-                    <h3>About me</h3>
-                    <div id="about">
-                         <p>A software engineer from Miami, enrolled at Stony Brook Univeristy for CS.</p>
-                         <a href="https://collinrijock.dev/Resume.pdf">Resume</a>
-                         <a href="https://github.com/collinrijock?tab=repositories">Github</a>
-                         <a href="https://www.linkedin.com/in/collin-rijock-62116a1b5/">LinkedIn</a>
-                    </div>
+               <div class="horizontal-div">
+                    <h1>Collin Rijock</h1>
+                    <h3>Software Developer</h3>
+                    <p>I'm a dev from Miami studying CS at Stony Brook University.</p>
                </div>
-               <div id="projects-container">
-                    <h3>Projects</h3>
-                    <!-- <Projects></Projects> -->
+               <div class="horizontal-div">
+                    <img src="./assets/me.png">
                </div>
-         </section>
+          </section>
+          <section>
+               <h1>Skills</h1>
+               <h1>Projects</h1>
+               <h1>Extras</h1>
+          </section>
      </div>
 </template>
 
 <script>
-global.jQuery = require("jquery");
-var $ = global.jQuery;
-window.$ = $;
-import anime from "animejs";
-import Parallax from "parallax-js";
 export default {
      data() {
-          return {};
+          return {
+          }
      },
      methods: {
-          initialAnimation() {
-               this.animateName();
-               this.initializeParallax();
-          },
-          animateName() {
-               anime({
-                    targets: "span",
-                    translateY: -100,
-                    duration: 1000,
-                    delay: anime.stagger(70),
-                    opacity: [{ value: 0, duration: 70 }, { value: 1 }],
-                    complete: (anim) => {
-                         $("#subtitle").css("opacity", "1");
-                    },
-               });
-          },
-          initializeParallax() {
-               var scene = $("#splash").get(0);
-               var parallaxInstance = new Parallax(scene);
-          },
      },
-     mounted() {
-          this.initialAnimation();
-     },
-};
+}
 </script>
 
 <style lang="scss">
-@import url("./assets/Tofino Pro Wide/tofino.css");
+$medium: #757575;
 .clear {
      margin: 0px;
      padding: 0px;
 }
 body {
      @extend .clear;
+     font-family: system, -apple-system, ".SFNSText-Regular", "San Francisco", "Roboto", "Segoe UI", "Helvetica Neue", "Lucida Grande", sans-serif;
+     color:#292929; 
 }
+
 #main-page {
-     overflow: hidden;
-     min-height: 100vh;
-     width: 100%;
-     background: #ffea46;
-     background-size: cover;
-     color: #191923;
-     #splash {
-          width: 100vw;
-          height: 100vh;
-          position: relative;
-          display: grid;
-          grid-template-columns: repeat(12, 1fr);
-          grid-template-rows: repeat(12, 1fr);
-          #logo {
-               height: 7vh;
-               grid-column: 2;
-               grid-row: 2;
-          }
-          .box {
-               opacity: 0.8;
-               height: 70vh;
-               grid-column: 7;
-               grid-row: 4;
-          }
-          #last-name {
-               grid-column: 10;
-               grid-row: 10;
-               display: flex !important;
-               flex-flow: row nowrap;
-               width: 50% !important;
-               height: 100% !important;
-               position: relative !important;
-               span {
-                    display: block;
-                    @extend .clear;
-                    font-family: "tofino";
-                    color: #191923;
-                    font-size: 4rem;
-                    opacity: 0;
-                    filter: drop-shadow(0 3px 3px rgb(129, 129, 129));
-               }
-          }
-          #first-name {
-               grid-column: 9;
-               grid-row: 10;
-               @extend #last-name;
-               padding-right: 2vw;
-          }
-          #subheader {
-               font-size: 1.7rem;
-               grid-column: 9 / span 10;
-               grid-row: 10;
-               font-family: "tofino";
-               color: #191923;
-               filter: drop-shadow(0 1px 1px rgb(129, 129, 129));
-          }
-     }
+     background: white;
+     width: 100vw;
+     display: flex;
+     flex-flow: column nowrap;
+     padding: 9vh 19vw;
+     box-sizing: border-box;
+
      #hero {
-          font-family: "Roboto", sans-serif;
           display: flex;
-          flex-flow: column nowrap;
-          align-items: center;
-          justify-content: space-around;
-          #about-container {
-               display: flex;
-               flex-flow: column nowrap;
-               align-items: center;
-               justify-content: space-around;
-               h3 {
+          flex-flow: row wrap;
+          .horizontal-div {
+               h1 {
+                    @extend .clear;
+                    font-size: 4rem;
                }
-               #about {
-                    display: flex;
-                    flex-flow: column nowrap;
-                    align-items: center;
-                    justify-content: space-around;
-                    p {
-                    }
-                    a {
-                    }
+               h3 {
+                    @extend h1;
+                    color: $medium;
+                    font-size: 2rem;
+                    font-weight: 600;
+                    margin-top: 15px;
+               }
+               img {
+                    height: 260px;
+                    border-radius: 7px;
                }
           }
-          #project-container {
-               display: flex;
-               flex-flow: column nowrap;
-               align-items: center;
-               justify-content: space-around;
-               h3 {
-               }
-          }
+         .horizontal-div + .horizontal-div {
+              margin-left: auto;
+              margin-top: 15px;
+         } 
      }
 }
+
 </style>
